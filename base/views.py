2153ac31adff8fd7bc558007c060dde08b9db835
Prasanth Anand P <prasanthanand2002@gmail.com>
2023-08-08 23:16:59 +0530

    initial commit

---
+++ base/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
+
+# rooms = [
+#     {'id': 1, 'name': 'A'},
+#     {'id': 2, 'name': 'B'},
+#     {'id': 3, 'name': 'C'},
+# ]
+
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request,'base/home.html', {'rooms': rooms})
+
+def room(request,pk):
+    room = Room.objects.get(id = pk)
+    context = {'room': room}
+    return render(request,'base/room.html', context)
